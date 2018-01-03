a6e7e441a8bf19c35a3a8befc22617f52590acd7
Emily De Leon <Edeleon8425@conestogac.on.ca>
2018-01-03 10:34:11 -0500

    added options menu to action bar

--- app/src/main/java/com/example/edeleon8425/popfizzle/PFhomePage.java
+++ app/src/main/java/com/example/edeleon8425/popfizzle/PFhomePage.java
@@ -3,6 +3,8 @@ package com.example.edeleon8425.popfizzle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,10 +19,34 @@ public class PFhomePage extends AppCompatActivity {
     SQLiteDB db=null;
     ArrayList<News> use=null;
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.item1:
+                //your action
+                break;
+            case R.id.item2:
+                Intent intent = new Intent(PFhomePage.this, PFLogin.class);
+                startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pfhome_page);
-
         db= new SQLiteDB(this);
 
         use=db.getData();
